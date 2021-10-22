8d194e062870ce7c9ddef716a51146bcc1a7ae38
Ashley (She) <aadamson@hashicorp.com>
2021-10-22 11:43:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 variable "environment" {
     default = ""
-    description = "The notnot this is a test of the environment in which you would like to provision"
+    description = "The is a fork test this is a test of the environment in which you would like to provision"
 }
 
 resource "random_pet" "host" {
